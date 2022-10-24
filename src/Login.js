603b161c37453d0a7c0307de746b18b747405168
Anirudh Patel <anirudh26p@gmail.com>
2022-10-24 17:18:21 +0530

    added-styles

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,11 @@
 import React from "react";
 import '@coreui/coreui/dist/css/coreui.min.css'
-import { CForm, CRow, CFormLabel, CCol, CButton, CFormInput, CFormSelect } from "@coreui/react";
+import { CForm, CRow, CFormLabel, CCol, CButton, CFormInput, CFormSelect, CInputGroup, CDropdown, CDropdownItem, CDropdownDivider, CDropdownMenu, CDropdownToggle } from "@coreui/react";
+import './login.css'
+
 export default function Login() {
     const [loginPage, setLoginpage] = React.useState(true);
-    const [city, setCity] = React.useState([{name: 'Surat'},{name: 'Munbai'}]);
+    const [city, setCity] = React.useState([{ name: 'Surat' }, { name: 'Munbai' }]);
     const [parking, setParking] = React.useState([]);
     const [selectCity, setSelectCity] = React.useState('');
 
@@ -14,56 +16,74 @@ export default function Login() {
     return (
         <>
             {loginPage === true ? (
-                <div style={{ width: '40%', minWidth: '400px', height: 'max-content', margin: 'auto', position: 'relative', top: '30vh', borderRadius: '10px', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'blue' }}>
-                    <CForm style={{ width: '95%', height: '70%', color: 'white' }}>
-                        <h3 style={{ width: '100%', textAlign: 'center', padding: '1rem 0' }}>Login</h3>
-                        <CRow className="mb-3">
-                            <CFormLabel htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</CFormLabel>
-                            <CCol sm={10} >
-                                <CFormInput type="email" id="inputEmail3" />
+                <div className="root-form">
+                    <img className="img" src="./background.jpg"></img>
+                    <CForm className="form">
+                        <p className="heading">Login</p>
+                        <br></br>
+                        <CRow className="mb-3 row">
+                            <CFormLabel htmlFor="inputNumber3" className="col-sm-4 col-form-label">Mobile No. :</CFormLabel>
+                            <CCol sm={6} >
+                                <CInputGroup className="mb-3">
+                                    <CDropdown variant="input-group">
+                                        <CDropdownToggle color="secondary" variant="outline">+91</CDropdownToggle>
+                                        <CDropdownMenu>
+                                            <CDropdownItem href="#">+91</CDropdownItem>
+                                            <CDropdownItem href="#">+12</CDropdownItem>
+                                            <CDropdownItem href="#">+1</CDropdownItem>
+                                        </CDropdownMenu>
+                                    </CDropdown>
+                                    <CFormInput aria-label="Text input with dropdown button" />
+                                </CInputGroup>
                             </CCol>
                         </CRow>
-                        <CRow className="mb-3">
-                            <CFormLabel htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</CFormLabel>
-                            <CCol sm={10} >
+                        <CRow className="mb-3 row">
+                            <CFormLabel htmlFor="inputPassword3" className="col-sm-4 col-form-label">Password :</CFormLabel>
+                            <CCol sm={6} >
                                 <CFormInput type="password" id="inputPassword3" />
                             </CCol>
                         </CRow>
-                        <CButton type="submit" style={{ backgroundColor: 'white', color: 'grey', width: '100%', margin: '1rem 0' }}>Login</CButton>
-                        <p style={{ width: '100%', textAlign: 'center', margin: '1rem 0' }}>Don't have an account? &nbsp; <CButton color="info" onClick={(prev) => {
+                        <CButton type="submit" className="mb-3 btn">LogIn</CButton>
+                        <p>Don't have an account? &nbsp; <CButton className="btn" onClick={(prev) => {
                             setLoginpage(!prev)
                         }}>Sign Up</CButton></p>
                     </CForm>
                 </div>
             ) : (
-                <div style={{ width: '40%', minWidth: '400px', height: 'max-content', margin: 'auto', position: 'relative', top: '10vh', borderRadius: '10px', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'blue' }}>
-                    <CForm style={{ width: '95%', height: '70%', color: 'white' }}>
-                        <h3 style={{ width: '100%', textAlign: 'center', padding: '1rem 0' }}>Signup</h3>
-                        <CRow className="mb-3">
-                            <CFormLabel htmlFor="inputNumber3" className="col-sm-2 col-form-label">Mobile No.</CFormLabel>
-                            <CCol sm={10} >
-                                <CFormInput type="number" id="inputNumber3" />
+                <div className="root-form">
+                    <img className="img2" src="./background.jpg"></img>
+                    <CForm className="form">
+                        <p className="heading">Signup</p>
+                        <br></br>
+                        <CRow className="mb-3 row">
+                            <CFormLabel htmlFor="inputNumber3" className="col-sm-4 col-form-label">Mobile No. :</CFormLabel>
+                            <CCol sm={6} >
+                                <CInputGroup className="mb-3">
+                                    <CDropdown variant="input-group">
+                                        <CDropdownToggle color="secondary" variant="outline">+91</CDropdownToggle>
+                                        <CDropdownMenu>
+                                            <CDropdownItem href="#">+91</CDropdownItem>
+                                            <CDropdownItem href="#">+12</CDropdownItem>
+                                            <CDropdownItem href="#">+1</CDropdownItem>
+                                        </CDropdownMenu>
+                                    </CDropdown>
+                                    <CFormInput aria-label="Text input with dropdown button" />
+                                </CInputGroup>
                             </CCol>
                         </CRow>
-                        <CRow className="mb-3">
-                            <CFormLabel htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</CFormLabel>
-                            <CCol sm={10} >
-                                <CFormInput type="email" id="inputEmail3" />
-                            </CCol>
-                        </CRow>
-                        <CRow className="mb-3">
-                            <CFormLabel htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</CFormLabel>
+                        {/* <CRow className="mb-3 row">
+                            <CFormLabel htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password :</CFormLabel>
                             <CCol sm={10} >
                                 <CFormInput type="password" id="inputPassword3" />
                             </CCol>
                         </CRow>
-                        <CRow className="mb-3">
+                        <CRow className="mb-3 row">
                             <CFormLabel htmlFor="inputPassword4" className="col-sm-2 col-form-label">Re-enter Password</CFormLabel>
                             <CCol sm={10} >
                                 <CFormInput type="password" id="inputPassword4" />
                             </CCol>
                         </CRow>
-                        <CFormSelect aria-label="Default select example" className="mb-3" onChange={(e) => {
+                        <CFormSelect aria-label="Default select example" className="mb-3 select" onChange={(e) => {
                             setSelectCity(e.target.value)
                         }}>
                             <option>Select City</option>
@@ -73,14 +93,14 @@ export default function Login() {
                                 )
                             })}
                         </CFormSelect>
-                        <CFormSelect aria-label="Default select example" className="mb-3">
+                        <CFormSelect aria-label="Default select example" className="mb-3 select">
                             <option>Select Parking in {selectCity}</option>
                             {parking.map((value) => {
                                 <option value={value.name}>{value.name}</option>
                             })}
-                        </CFormSelect>
-                        <CButton type="submit" className="mb-3">Sign in</CButton>
-                        <p style={{ width: '100%', textAlign: 'center', margin: '1rem 0' }}>Already have an account? &nbsp; <CButton onClick={() => {
+                        </CFormSelect> */}
+                        <CButton type="submit" className="mb-3 btn">Sign in</CButton>
+                        <p>Already have an account? &nbsp; <CButton className="btn" onClick={() => {
                             setLoginpage(!loginPage)
                         }}>Login</CButton></p>
                     </CForm>

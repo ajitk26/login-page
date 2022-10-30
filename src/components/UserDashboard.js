f3bf2ef354184b6e26d5b9171b19e24f4152dc5c
Ajit kumar <aky8062@gmail.com>
2022-10-31 05:11:42 +0530

    updated

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -1,7 +1,23 @@
+import { CButton } from "@coreui/react";
 import React from "react";
+import { useState,useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const UserDashboard = () => {
-  return <div><h1>hello user</h1></div>;
+  const navigate=useNavigate()
+
+  useEffect(()=>{
+    if(!localStorage.getItem('token')){
+      navigate("/")
+    }
+  })
+  return <div>
+    <h1>hello user</h1>
+    <CButton onClick={()=>{
+      localStorage.removeItem('token')
+      navigate("/")
+    }}>Logout</CButton>
+    </div>;
 };
 
 export default UserDashboard;
